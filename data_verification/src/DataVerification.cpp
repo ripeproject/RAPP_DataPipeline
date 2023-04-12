b72323230ba9b2f99b2c4eb6bc13f279a2bf924d
Brett Feddersen <bfeddrsn@illinois.edu>
2023-04-12 10:29:03 -0500

    Made the default one thread for the thread pool

--- data_verification/src/DataVerification.cpp
+++ data_verification/src/DataVerification.cpp
@@ -26,7 +26,8 @@ int main(int argc, char** argv)
 {
 	using namespace std::filesystem;
 
-	int num_of_threads = 0;
+	// The default is to use only one thread as this application is I/O limited.
+	int num_of_threads = 1;
 	std::string input_directory = current_path().string();
 
 	auto cli = lyra::cli()
@@ -77,7 +78,7 @@ int main(int argc, char** argv)
 	num_of_threads = std::max(num_of_threads, 0);
 	num_of_threads = std::min(num_of_threads, max_threads);
 
-	// Constructs a thread pool with as many threads as available in the hardware.
+	// Constructs a thread pool with the desired number of threads.
 	BS::thread_pool pool(num_of_threads);
 	int n = pool.get_thread_count();
 
