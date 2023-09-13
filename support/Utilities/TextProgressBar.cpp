cdfe1918f7ae6af86d2be54b496437cd5758a97c
Brett Feddersen <bfeddrsn@illinois.edu>
2023-09-13 17:00:06 -0500

    Added a text based progress bar

---
+++ support/Utilities/TextProgressBar.cpp
@@ -0,0 +1,128 @@
+﻿
+#include "TextProgressBar.hpp"
+
+#include "StringUtils.hpp"
+
+#include <iostream>
+#include <format>
+#include <cmath>
+
+
+using namespace std;
+
+
+void cTextProgressBar::clear()
+{
+    mProgressEntries.clear();
+}
+
+void cTextProgressBar::setMaxID(int max_id)
+{
+    mMaxID = max_id;
+}
+
+void cTextProgressBar::setFillChars(char half_full, char full)
+{
+    mFullChar = full;
+    mHalfChar = half_full;
+}
+
+void cTextProgressBar::setLength(uint8_t length)
+{
+    mProgressLength = length;
+}
+
+void cTextProgressBar::addProgressEntry(int id, const std::string& prefix)
+{
+    std::string str = nStringUtils::compactFilename(prefix, 50);
+    mProgressEntries[id] = { 0, str };
+
+    std::string line;
+
+    if (mMaxID > 0)
+        line = format("({}/{}): ", id+1, mMaxID);
+
+    line += format("{:50s}   0% [", str);
+
+    for (int i = 0; i < mProgressLength; ++i)
+        line += mEmptyChar;
+
+    line += "]";
+
+    std::cout << line << std::flush;
+}
+
+void cTextProgressBar::updateProgressEntry(int id, const std::string& prefix, float progress_pct)
+{
+    auto entry = mProgressEntries[id];
+    entry.prefix = nStringUtils::compactFilename(prefix, 50);
+    mProgressEntries[id] = entry;
+
+    updateProgressEntry(id, progress_pct);
+}
+
+void cTextProgressBar::updateProgressEntry(int id, float progress_pct)
+{
+    auto& entry = mProgressEntries[id];
+
+    float integer = 0.0f;
+    auto num = (progress_pct / 100.0f) * mProgressLength;
+    auto fraction = std::modf(num, &integer);
+    int num_fill_char = static_cast<int>(integer);
+
+    bool needs_half_fill = (10.0f * fraction) >= 5.0f;
+
+    if (needs_half_fill)
+    {
+        needs_half_fill = true;
+    }
+
+    if ((num_fill_char == entry.num_fill_char) && (needs_half_fill == entry.needs_half_fill))
+        return;
+
+    entry.num_fill_char = num_fill_char;
+    entry.needs_half_fill = needs_half_fill;
+
+    std::string line = "\r";
+
+    if (mMaxID > 0)
+        line += format("({}/{}): ", id + 1, mMaxID);
+
+    line += format("{:50s} {:3}% [", entry.prefix, static_cast<int>(progress_pct));
+
+    for (int i = 0; i < num_fill_char; ++i)
+        line += mFullChar;
+
+    if (needs_half_fill)
+    {
+        line += mHalfChar;
+        ++num_fill_char;
+    }
+
+    for (int i = 0; i < (mProgressLength - num_fill_char); ++i)
+        line += mEmptyChar;
+
+    line += "]";
+
+    std::cout << line << std::flush;
+}
+
+void cTextProgressBar::finishProgressEntry(int id)
+{
+    auto entry = mProgressEntries[id];
+
+    std::string line = "\r";
+
+    if (mMaxID > 0)
+        line += format("({}/{}): ", id + 1, mMaxID);
+
+    line += format("{:50s}      [", entry.prefix);
+
+    for (int i = 0; i < mProgressLength; ++i)
+        line += mFullChar;
+
+    line += "]";
+
+    std::cout << line << std::endl;
+}
+
---
+++ support/Utilities/TextProgressBar.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include <map>
+
+
+class cTextProgressBar
+{
+public:
+	cTextProgressBar() = default;
+
+	void clear();
+
+	void setMaxID(int max_id);
+
+	void setFillChars(char half_full, char full);
+	void setLength(uint8_t length);
+
+	void addProgressEntry(int id, const std::string& prefix);
+	void updateProgressEntry(int id, const std::string& prefix, float progress_pct);
+	void updateProgressEntry(int id, float progress_pct);
+	void finishProgressEntry(int id);
+
+private:
+	int mMaxID = -1;
+
+	uint8_t mProgressLength = 50;
+
+	char mEmptyChar = ' ';
+	char mFullChar  = '=';
+	char mHalfChar  = '-';
+
+	struct sLineInfo_t
+	{
+		int line_number = 0;
+		std::string prefix;
+		int num_fill_char = 0;
+		bool needs_half_fill = false;
+	};
+
+	std::map<int, sLineInfo_t> mProgressEntries;
+};
