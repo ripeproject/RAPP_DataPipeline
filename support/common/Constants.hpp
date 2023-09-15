2270df95da07b6883a921b606f21140c43348e5d
Brett Feddersen <bfeddrsn@illinois.edu>
2023-09-15 15:31:01 -0500

    Initial commit

---
+++ support/common/Constants.hpp
@@ -0,0 +1,23 @@
+
+#pragma once
+
+#include <cmath>
+
+namespace nConstants
+{
+	const static double PI = 2.0 * std::asin(+1.0);
+	const static double TWO_PI = 2.0 * PI;
+
+	const static double RAD_TO_DEG = 180.0 / PI;
+	const static double DEG_TO_RAD = 1.0 / RAD_TO_DEG;
+
+	const static double M_TO_MM = 1000.0;
+	const static double MM_TO_M = 1.0 / M_TO_MM;
+	const static double M_TO_CM = 100.0;
+	const static double CM_TO_M = 1.0 / M_TO_CM;
+	const static double CM_TO_MM = 10.0;
+	const static double MM_TO_CM = 1.0 / CM_TO_MM;
+
+	const static double MPS_TO_MPH = 2.23693629;
+	const static double MPH_TO_MPS = 1.0 / MPS_TO_MPH;
+}
