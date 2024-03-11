1cebc5a49453c3856da1755587c1d475e1b016c6
Brett Feddersen <bfeddrsn@illinois.edu>
2024-03-11 14:27:25 -0500

    Added more constants

--- support/common/Constants.hpp
+++ support/common/Constants.hpp
@@ -1,11 +1,13 @@
 
 #pragma once
 
+#include <numbers>
 #include <cmath>
 
 namespace nConstants
 {
-	const static double PI = 2.0 * std::asin(+1.0);
+//	const static double PI = 2.0 * std::asin(+1.0);
+	const static double PI = std::numbers::pi;
 	const static double TWO_PI = 2.0 * PI;
 
 	/*** Angle Conversions ***/
@@ -20,13 +22,27 @@ namespace nConstants
 	const static double CM_TO_MM = 10.0;
 	const static double MM_TO_CM = 1.0 / CM_TO_MM;
 
+	const static double FT_TO_M = 0.3048;
+	const static double M_TO_FT = 1.0 / FT_TO_M;
+
+	const static double FT_TO_MM = FT_TO_M * M_TO_MM;
+	const static double MM_TO_FT = 1.0 / FT_TO_MM;
+
 	/*** Time Conversions ***/
 	const static double SEC_TO_MS = 1000.0;
 	const static double MS_TO_SEC = 1.0 / SEC_TO_MS;
 	const static double MS_TO_US = 1000.0;
-	const static double US_TO_MS = 1.0 / MS_TO_US;
+	const static double MS_TO_NS  = 1'000'000.0;
+
 	const static double SEC_TO_US = SEC_TO_MS * MS_TO_US;
 	const static double US_TO_SEC = 1.0 / SEC_TO_US;
+	const static double US_TO_MS  = 1.0 / MS_TO_US;
+	const static double US_TO_NS  = 1000.0;
+
+	const static double SEC_TO_NS = 1'000'000'000.0;
+	const static double NS_TO_SEC = 1.0 / SEC_TO_NS;
+	const static double NS_TO_US  = 1.0 / US_TO_NS;
+	const static double NS_TO_MS  = 1.0 / MS_TO_NS;
 
 	/*** Speed Conversions ***/
 	const static double MPS_TO_MPH = 2.23693629;
