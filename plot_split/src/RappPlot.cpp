267759e9c4363177e6479588867c7499f0bd99a0
Brett Feddersen <bfeddrsn@illinois.edu>
2024-03-14 08:49:31 -0500

    Initial commit

---
+++ plot_split/src/RappPlot.cpp
@@ -0,0 +1,125 @@
+
+#include "RappPlot.hpp"
+
+#include <numbers>
+#include <algorithm>
+#include <numeric>
+
+
+cRappPlot::cRappPlot(int plotId) : mID(plotId)
+{}
+
+cRappPlot::~cRappPlot()
+{}
+
+
+int cRappPlot::id() const { return mID; }
+const std::string& cRappPlot::plotName() const { return mName; }
+const std::string& cRappPlot::event() const { return mEvent; }
+const std::string& cRappPlot::eventDescription() const { return mEventDescription; }
+
+
+bool cRappPlot::empty() const
+{
+	return mCloud.empty(); 
+}
+
+
+void cRappPlot::setPlotName(const std::string& name)
+{
+	mName = name;
+}
+
+void cRappPlot::setEvent(const std::string& event)
+{
+	mEvent = event;
+}
+
+void cRappPlot::setEventDescription(const std::string& description)
+{
+	mEventDescription = description;
+}
+
+
+void cRappPlot::setPointCloud(const cRappPointCloud& pointCloud)
+{
+	mCloud.clear();
+
+	mCloud = pointCloud;
+}
+
+rfm::sPoint3D_t cRappPlot::getPoint(int x_mm, int y_mm, int r_mm) const
+{
+	rfm::sPoint3D_t result = {x_mm, y_mm, rfm::INVALID_HEIGHT};
+
+	auto lb = result;
+	lb.x_mm -= r_mm;
+
+	auto first = std::lower_bound(mCloud.begin(), mCloud.end(), lb,
+		[](const rfm::sPoint3D_t& a, rfm::sPoint3D_t value)
+		{
+			return a.x_mm < value.x_mm;
+		});
+
+	if (first == mCloud.end())
+		return result;
+
+	auto ub = result;
+	ub.x_mm += r_mm;
+
+	auto last = std::upper_bound(mCloud.begin(), mCloud.end(), ub,
+		[](const rfm::sPoint3D_t& a, rfm::sPoint3D_t value)
+		{
+			return a.x_mm < value.x_mm;
+		});
+
+	std::vector<rfm::sPoint3D_t>  xs(first, last);
+
+	lb = result;
+	lb.y_mm -= r_mm;
+
+	first = std::lower_bound(xs.begin(), xs.end(), lb,
+		[](const rfm::sPoint3D_t& a, rfm::sPoint3D_t value)
+		{
+			return a.y_mm < value.y_mm;
+		});
+
+	if (first == xs.end())
+		return result;
+
+	ub = result;
+	ub.y_mm += r_mm;
+
+	last = std::upper_bound(xs.begin(), xs.end(), ub,
+		[](const rfm::sPoint3D_t& a, rfm::sPoint3D_t value)
+		{
+			return a.y_mm < value.y_mm;
+		});
+
+	if (first == last)
+		return *first;
+
+	std::vector<rfm::sPoint3D_t>  points(first, last);
+
+	if (points.empty())
+		return result;
+
+	std::size_t n = 0;
+	int z = 0;
+	for (const auto& point : points)
+	{
+		if (point.z_mm != rfm::INVALID_HEIGHT)
+		{
+			z += point.z_mm;
+			++n;
+		}
+	}
+
+	if (n > 0)
+		result.z_mm = z / n;
+
+	return result;
+}
+
+
+
---
+++ plot_split/src/RappPlot.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "RappPointCloud.hpp"
+
+#include "Constants.hpp"
+
+#include <vector>
+#include <string>
+
+
+/**
+ * Class to store basic plot information and point cloud
+ */
+class cRappPlot
+{
+public:
+	typedef cRappPointCloud::value_type					value_type;
+	typedef cRappPointCloud::size_type					size_type;
+	typedef cRappPointCloud::difference_type			difference_type;
+	typedef cRappPointCloud::reference					reference;
+	typedef cRappPointCloud::const_reference			const_reference;
+	typedef cRappPointCloud::pointer					pointer;
+	typedef cRappPointCloud::const_pointer				const_pointer;
+	typedef cRappPointCloud::iterator					iterator;
+	typedef cRappPointCloud::const_iterator				const_iterator;
+	typedef cRappPointCloud::reverse_iterator			reverse_iterator;
+	typedef cRappPointCloud::const_reverse_iterator		const_reverse_iterator;
+
+public:
+	explicit cRappPlot(int plotId);
+	cRappPlot(const cRappPlot& pc) = default;
+	~cRappPlot();
+
+	int id() const;
+
+	const std::string& plotName() const;
+
+	const std::string& event() const;
+	const std::string& eventDescription() const;
+
+
+    bool empty() const;
+
+	void setPlotName(const std::string& name);
+
+	void setEvent(const std::string& event);
+	void setEventDescription(const std::string& description);
+
+	void setPointCloud(const cRappPointCloud& pointCloud);
+
+
+	rfm::sPoint3D_t getPoint(int x_mm, int y_mm, int r_mm) const;
+
+	iterator begin() { return mCloud.begin(); }
+	const_iterator begin() const { return mCloud.begin(); }
+	const_iterator cbegin() { return mCloud.cbegin(); }
+
+	iterator end() { return mCloud.end(); }
+	const_iterator end() const { return mCloud.end(); }
+	const_iterator cend() { return mCloud.cend(); }
+
+	reverse_iterator rbegin() { return mCloud.rbegin(); }
+	const_reverse_iterator rbegin() const { return mCloud.rbegin(); }
+	const_reverse_iterator crbegin() { return mCloud.crbegin(); }
+
+	reverse_iterator rend() { return mCloud.rend(); }
+	const_reverse_iterator rend() const { return mCloud.rend(); }
+	const_reverse_iterator crend() { return mCloud.crend(); }
+
+	rfm::sPoint3D_t operator[](int i) const { return mCloud[i]; }
+
+    const cRappPointCloud& data() const { return mCloud; }
+
+private:
+	int mID;
+
+	std::string mName;
+
+	std::string mEvent;
+	std::string mEventDescription;
+
+	
+	cRappPointCloud mCloud;
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Implementation Details
+///////////////////////////////////////////////////////////////////////////////
+
