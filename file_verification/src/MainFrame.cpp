29040dfe7cd39a4f9a96362651bb5d2456b2f64d
Brett Feddersen <bfeddrsn@illinois.edu>
2024-08-15 10:29:55 -0500

    Shrank default windows size

--- file_verification/src/MainFrame.cpp
+++ file_verification/src/MainFrame.cpp
@@ -93,8 +93,8 @@ cMainFrame::cMainFrame()
 	SetBackgroundStyle(wxBG_STYLE_SYSTEM);
 
 	auto size = GetBestSize();
-	size.IncBy(350);
-	size.SetHeight(650);
+	size.IncBy(250);
+	size.SetHeight(450);
 	SetMinSize(size);
 
 	Centre();
