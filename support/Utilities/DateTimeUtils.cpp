2991a1a12dae1a0ae6f8c83e02a95aaebb04985d
Brett Feddersen <bfeddrsn@illinois.edu>
2024-10-30 17:04:24 -0500

    Added another to_day_of_year function

--- support/Utilities/DateTimeUtils.cpp
+++ support/Utilities/DateTimeUtils.cpp
@@ -26,6 +26,24 @@ std::time_t nDateUtils::to_time_t(const std::string& month, const std::string& d
 	return mktime(&date);
 }
 
+std::time_t nDateUtils::to_time_t(int month, int day, int year)
+{
+	if ((month < 1) || (month > 12))
+		return { 0 };
+	if ((day < 1) || (day > 31))
+		return { 0 };
+	if (year < 1900)
+		return { 0 };
+
+	struct tm date = { 0 };
+	date.tm_year = year - 1900;
+	date.tm_mon = month - 1;
+	date.tm_mday = day;
+	date.tm_isdst = -1;
+
+	return mktime(&date);
+}
+
 std::uint16_t nDateUtils::to_day_of_year(std::time_t date)
 {
     auto tm = localtime(&date);
@@ -33,6 +51,12 @@ std::uint16_t nDateUtils::to_day_of_year(std::time_t date)
 }
 
 std::uint16_t nDateUtils::to_day_of_year(const std::string& month, const std::string& day, const std::string& year)
+{
+	std::time_t date = to_time_t(month, day, year);
+	return to_day_of_year(date);
+}
+
+std::uint16_t nDateUtils::to_day_of_year(int month, int day, int year)
 {
 	std::time_t date = to_time_t(month, day, year);
 	return to_day_of_year(date);
--- support/Utilities/DateTimeUtils.hpp
+++ support/Utilities/DateTimeUtils.hpp
@@ -6,11 +6,28 @@
 
 namespace nDateUtils
 {
-    std::time_t to_time_t(const std::string& month, const std::string& day, const std::string& year);
+	/*
+	* Converts month, day, and year strings to time_t
+	*
+	* Month is a string representing an integer in the range 1 to 12
+	* Day is a string representing an integer in the range 1 to 31 (depends on month)
+	* Year is a string representing an integer greater than 1900
+	*/
+	std::time_t to_time_t(const std::string& month, const std::string& day, const std::string& year);
+
+	/*
+	* Converts month, day, and year integers to time_t
+	* 
+	* Month is an integer in the range 1 to 12
+	* Day is an integer in the range 1 to 31 (depends on month)
+	* Year is an integer greater than 1900 
+	*/
+	std::time_t to_time_t(int month, int day, int year);
 
 	std::uint16_t to_day_of_year(std::time_t date);
 
 	std::uint16_t to_day_of_year(const std::string& month, const std::string& day, const std::string& year);
-	
+	std::uint16_t to_day_of_year(int month, int day, int year);
+
 }
 
