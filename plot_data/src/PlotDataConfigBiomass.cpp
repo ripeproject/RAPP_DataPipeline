651cf7fc080c5b3303e4452ff6d98997deff852c
Brett Feddersen <bfeddrsn@illinois.edu>
2024-11-01 16:30:26 -0500

    Initial commit

---
+++ plot_data/src/PlotDataConfigBiomass.cpp
@@ -0,0 +1,78 @@
+
+#include "PlotDataConfigBiomass.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <array>
+#include <algorithm>
+#include <stdexcept>
+
+const char* BIOMASS_SECTION_NAME = "above-ground biomass";
+
+cPlotDataConfigBiomass::cPlotDataConfigBiomass()
+{}
+
+void cPlotDataConfigBiomass::clear()
+{
+	mDirty = false;
+
+	mGroundLevelBound_mm = 0.0;
+	mVoxelSize_mm = 1.0;
+}
+
+bool cPlotDataConfigBiomass::isDirty() const
+{
+	return mDirty;
+}
+
+double cPlotDataConfigBiomass::getGroundLevelBound_mm() const
+{
+	return mGroundLevelBound_mm;
+}
+
+double cPlotDataConfigBiomass::getVoxelSize_mm() const
+{
+	return mVoxelSize_mm;
+}
+
+void cPlotDataConfigBiomass::setGroundLevelBound_mm(double ground_level_mm)
+{
+	mDirty |= (mGroundLevelBound_mm != ground_level_mm);
+	mGroundLevelBound_mm = ground_level_mm;
+}
+
+void cPlotDataConfigBiomass::setVoxelSize_mm(double voxel_size_mm)
+{
+	mDirty |= (mVoxelSize_mm != voxel_size_mm);
+	mVoxelSize_mm = voxel_size_mm;
+}
+
+void cPlotDataConfigBiomass::setDirty(bool dirty)
+{
+	mDirty = dirty;
+}
+
+void cPlotDataConfigBiomass::load(const nlohmann::json& jdoc)
+{
+	if (!jdoc.contains(BIOMASS_SECTION_NAME))
+		return;
+		
+	auto height = jdoc[BIOMASS_SECTION_NAME];
+
+	mGroundLevelBound_mm = height["ground level bound (mm)"];
+	mVoxelSize_mm = height["voxel size (mm)"];
+}
+
+void cPlotDataConfigBiomass::save(nlohmann::json& jdoc)
+{
+	nlohmann::json height;
+
+	height["ground level bound (mm)"] = mGroundLevelBound_mm;
+	height["voxel size (mm)"] = mVoxelSize_mm;
+
+	mDirty = false;
+
+	jdoc[BIOMASS_SECTION_NAME] = height;
+}
+
+
---
+++ plot_data/src/PlotDataConfigBiomass.hpp
@@ -0,0 +1,46 @@
+
+#pragma once
+
+#include "datatypes.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <utility>
+#include <vector>
+#include <string>
+#include <memory>
+
+
+class cPlotDataConfigBiomass
+{
+public:
+	cPlotDataConfigBiomass();
+	~cPlotDataConfigBiomass() = default;
+
+	void clear();
+
+	bool isDirty() const;
+
+	double getGroundLevelBound_mm() const;
+	double getVoxelSize_mm() const;
+
+	void setGroundLevelBound_mm(double ground_level_mm);
+	void setVoxelSize_mm(double voxel_size_mm);
+
+protected:
+	void setDirty(bool dirty);
+
+	void load(const nlohmann::json& jdoc);
+	void save(nlohmann::json& jdoc);
+
+private:
+	bool mDirty = false;
+
+	double mGroundLevelBound_mm = 0.0;
+
+	double mVoxelSize_mm = 1.0;
+
+	friend class cPlotDataConfigFile;
+};
+
+
