a97e8e639309fdfaec1ffaa66f8b855ff64076a5
Brett Feddersen <brett.feddersen@outlook.com>
2025-01-07 08:46:42 -0600

    initial commit

---
+++ support/PlotUtils/PlotSplitDataTypes.hpp
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include "datatypes.hpp"
+
+
+enum class ePlotOrientation { eWEST_TO_EAST, eEAST_TO_WEST, eNORTH_TO_SOUTH, eSOUTH_TO_NORTH };
+enum class ePlotIsolationMethod { NONE, CENTER_OF_PLOT, CENTER_OF_HEIGHT, POUR, ITERATIVE, FIND_CENTER };
+
+
+namespace rfm
+{
+	struct sPlotBoundingBox_t
+	{
+		int32_t	plotNumber = 0;
+		rappPoint2D_t northEastCorner;
+		rappPoint2D_t northWestCorner;
+		rappPoint2D_t southEastCorner;
+		rappPoint2D_t southWestCorner;
+	};
+}
+
