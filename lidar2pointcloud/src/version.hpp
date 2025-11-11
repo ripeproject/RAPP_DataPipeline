29fd3cdc0f9a18a6003e407bb5c102eaf2d609e1
Brett Feddersen <bfeddrsn@illinois.edu>
2025-11-11 10:31:03 -0600

    Initial commit

---
+++ lidar2pointcloud/src/version.hpp
@@ -0,0 +1,8 @@
+#include <string>
+
+std::string getVersionString();
+
+unsigned int getMajorVersion();
+unsigned int getMinorVersion();
+unsigned int getPatchVersion();
+unsigned int getTweakVersion();
